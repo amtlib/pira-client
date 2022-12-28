137f0ad54130ca0a2e86917e9dc0b1a984abb124
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-12-28 04:58:27 +0100

    Use task card, get data from the api

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,55 +1,112 @@
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Board from 'react-trello'
+import { TaskCard } from "../../components/TaskCard/TaskCard";
 
 import { UserContext } from "../../contexts/UserContext";
 import { BasicLayout } from "../../layouts/BasicLayout";
 
-const data = {
-    lanes: [
-      {
-        id: 'lane1',
-        title: 'Backlog',
-        cards: [
-          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
-          {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-        ]
-      },
-      {
-        id: 'lane2',
-        title: 'In development',
-        cards: []
-      },
-      {
-        id: 'lane3',
-        title: 'In testing',
-        cards: []
-      },
-      {
-        id: 'lane4',
-        title: 'In approval',
-        cards: []
-      },
-      {
-        id: 'lane5',
-        title: 'Done',
-        cards: []
-      }
-    ]
-  }
+const PROJECT = gql`
+    query PROJECT($id: ID) {
+        project(where: {id: $id}) {
+            id
+            title
+            tasks {
+              id
+              name
+              priority
+              assignedUser {
+                firstName
+                lastName
+              }
+              createdBy {
+                firstName
+                lastName
+              }
+              status
+              dueDate
+              description
+              
+            }
+          }
+    }
+`
 
-export default function PollPage() {
+
+export default function ProjectPage() {
     const router = useRouter();
 
     const { id } = router.query;
     const { userId } = useContext(UserContext);
-    
+    const { data, loading } = useQuery(PROJECT, { variables: { id } });
+
+    const [tasks, setTasks] = useState([
+        {
+            id: 'backlog',
+            title: 'Backlog',
+            cards: [
+                // { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false },
+                { id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' }, hideCardDeleteIcon: true }
+            ]
+        },
+        {
+            id: 'in-development',
+            title: 'In development',
+            cards: []
+        },
+        {
+            id: 'in-testing',
+            title: 'In testing',
+            cards: []
+        },
+        {
+            id: 'in-approval',
+            title: 'In approval',
+            cards: []
+        },
+        {
+            id: 'done',
+            title: 'Done',
+            cards: []
+        }
+    ])
+
+    // const { project } = data;
+
+    useEffect(() => {
+        if (data?.project?.tasks) {
+            const { tasks: apiTasks } = data.project;
+            setTasks(prev =>
+                prev.map(column =>
+                ({
+                    ...column, cards: apiTasks.filter(apiTask => apiTask.status === column.id)
+                        .map(task => (
+                            {
+                                id: task.id,
+                                title: task.name,
+                                description: task.description,
+                                label: task.priority,
+                                createdBy: `${task.createdBy.firstName.substring(0, 1).toUpperCase()}. ${task.createdBy.lastName.toUpperCase()}`,
+                                assignedTo: `${task.assignedUser.firstName.substring(0, 1).toUpperCase()}. ${task.assignedUser.lastName.toUpperCase()}`,
+                            }
+                        ))
+                })))
+        }
+    }, [data])
+
+    useEffect(() => {
+        console.log(data)
+        if (!data?.project && !loading) {
+            router.push("/");
+        }
+    }, [id, loading, data])
+
     return (
         <BasicLayout>
-            <Board data={data} />
+            <Board data={{ lanes: tasks }} hideCardDeleteIcon={true} components={{ Card: TaskCard }} />
         </BasicLayout>
     )
-    
+
 }
