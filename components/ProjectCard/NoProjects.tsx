609dc6ced774ccb997c8b706b0357a4d97e726fb
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-12-28 01:31:19 +0100

    Create no projects component

---
+++ components/ProjectCard/NoProjects.tsx
@@ -0,0 +1,25 @@
+import CryingCat from "../../public/images/cryingcat.jpeg";
+import Image from 'next/image';
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        width: "100%",
+        height: "calc(100vh - 60px)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+});
+
+export const NoProjects = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Image src={CryingCat.src} width="800px" height="450px" />
+            <h1>Create a new project or ask your coworker to assign you to one</h1>
+            <Button variant="contained">Create project!</Button>
+        </div>
+    );
+}
