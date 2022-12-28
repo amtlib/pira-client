7fec600956da9a8ed3024d5bb83bef1c65c3ea59
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-12-28 04:58:16 +0100

    Create task card

---
+++ components/TaskCard/TaskCard.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Wrapper = styled.div`
+    background-color: white;
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 5px;
+`;
+
+const Title = styled.h3`
+    font-size: 16px;
+    &:hover {
+        color: blue;
+    }
+`;
+
+const Label = styled.span<{ priority: "low" | "medium" | "high" }>`
+    color: white;
+    padding: 2px 5px;
+    border-radius: 3px;
+    ${({ priority }) => priority === "low" ? css`
+        background-color: green;
+    ` : priority === "medium" ? css`
+        background-color: yellow;
+    ` : css`
+        background-color: red;
+    `}
+`;
+
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const Person = styled.div``
+
+export const TaskCard = ({
+    onClick,
+    title,
+    assignedTo,
+    createdBy,
+    label,
+    reporter,
+    contributors,
+}) => {
+    return (
+        <Wrapper onClick={onClick}>
+            <Row>
+                <Person>Created by {createdBy}</Person>
+                <Label priority={label}>{label}</Label>
+            </Row>
+            <Row>
+                <Link href="#">
+                    <Title>{title}</Title>
+                </Link>
+            </Row>
+            <Row>
+                <Person>Assigned to {assignedTo}</Person>
+            </Row>
+        </Wrapper>
+    );
+};
