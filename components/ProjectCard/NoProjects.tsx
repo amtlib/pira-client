127c7fcc55e4e60dc8b8440c62d6f68aad79a918
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-12-29 16:00:05 +0100

    Add add new project button to the no projects component

--- components/ProjectCard/NoProjects.tsx
+++ components/ProjectCard/NoProjects.tsx
@@ -1,6 +1,8 @@
 import CryingCat from "../../public/images/cryingcat.jpeg";
 import Image from 'next/image';
 import { Button, makeStyles } from "@material-ui/core";
+import { ModalContext } from "../../contexts/ModalContext";
+import { useContext } from "react";
 
 const useStyles = makeStyles({
     root: {
@@ -15,11 +17,12 @@ const useStyles = makeStyles({
 
 export const NoProjects = () => {
     const classes = useStyles();
+    const { setIsCreateProjectModalOpen } = useContext(ModalContext);
     return (
         <div className={classes.root}>
             <Image src={CryingCat.src} width="800px" height="450px" />
             <h1>Create a new project or ask your coworker to assign you to one</h1>
-            <Button variant="contained">Create project!</Button>
+            <Button variant="contained" onClick={() => setIsCreateProjectModalOpen(true)}>Create project!</Button>
         </div>
     );
 }
