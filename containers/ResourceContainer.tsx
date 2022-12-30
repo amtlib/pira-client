03ef3f1d8b7e55c195c7dd1f91a6d6a5c3b8bdf8
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-12-30 03:09:32 +0100

    Get users assigned to project

--- containers/ResourceContainer.tsx
+++ containers/ResourceContainer.tsx
@@ -1,17 +1,42 @@
-import { useState } from "react";
-import { ResourceContext } from "../contexts/ResourceContext";
+import { gql, useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { ResourceContext, UserType } from "../contexts/ResourceContext";
+
+
+const ASSIGNED_USERS = gql`
+    query GetAssignedUsers($projectId: ID) {
+        assigneeUsers(where: {project: {id: {equals: $projectId}}}) {
+            user {
+                firstName
+                lastName
+                id
+            }
+        }
+    }
+`
 
 
 export function ResourceContainer({ children }) {
     const [activeProjectId, setActiveProjectId] = useState<string | null>(null);
-    const [activeTaskId, setActiveTaskId] = useState<string | null>(null);
-    
 
+    const [activeProjectAssigneeUsers, setActiveProjectAssigneeUsers] = useState<UserType[]>([])
+    const [activeTaskId, setActiveTaskId] = useState<string | null>(null);
+
+    const { data } = useQuery(ASSIGNED_USERS, { variables: { projectId: activeProjectId } });
+
+    useEffect(() => {
+        console.log("assigned users", data?.assigneeUsers.map(({ user }) => user.firstName))
+        if (data) {
+            setActiveProjectAssigneeUsers(data?.assigneeUsers.map(({ user }) => user as UserType));
+        }
+    }, [data])
 
     return <ResourceContext.Provider value={{
         activeProjectId,
         setActiveProjectId,
         activeTaskId,
-        setActiveTaskId
+        setActiveTaskId,
+        activeProjectAssigneeUsers,
+        setActiveProjectAssigneeUsers
     }}>{children}</ResourceContext.Provider>
 }
--- contexts/ResourceContext.ts
+++ contexts/ResourceContext.ts
@@ -1,15 +1,25 @@
 import { createContext } from "react";
 
+export type UserType = {
+    firstName: string;
+    lastName: string;
+    id: string;
+}
+
 export type ResourceContextType = {
     activeProjectId: string | null;
     setActiveProjectId: (id: string) => void;
     activeTaskId: string | null;
     setActiveTaskId: (id: string) => void;
+    activeProjectAssigneeUsers: UserType[];
+    setActiveProjectAssigneeUsers: (users: UserType[]) => void;
 };
 
 export const ResourceContext = createContext<ResourceContextType>({
     activeProjectId: null,
     activeTaskId: null,
     setActiveProjectId: () => {},
-    setActiveTaskId: () => {}
+    setActiveTaskId: () => {},
+    activeProjectAssigneeUsers: [],
+    setActiveProjectAssigneeUsers: () => {}
 });
