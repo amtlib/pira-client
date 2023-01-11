55c78759e807f9db35d688fe057e2a630f24692e
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-11 17:17:06 +0100

    Limit page width

--- layouts/BasicLayout.tsx
+++ layouts/BasicLayout.tsx
@@ -2,16 +2,28 @@ import { Footer } from "../components/Footer/Footer"
 import { CreateProjectModal } from "../components/Modals/CreateProjectModal"
 import { CreateSubtaskModal } from "../components/Modals/CreateSubtaskModal"
 import { CreateTaskModal } from "../components/Modals/CreateTaskModal"
-import Navbar from "../components/Navigation/Navigation"
+import Navbar from "../components/Navigation/Navigation";
+import styled from "styled-components"
+import React from "react";
+
+const Wrapper = styled.div`
+    margin: 20px;
+    max-width: 960px;
+    margin: 0 auto;
+`
 
 export const BasicLayout = ({ children, page }) => {
+    const WrapperComponent = ["project", "login", "register", "projects"].includes(page) ? React.Fragment : Wrapper;
     return (
         <>
             <Navbar page={page} />
-            {children}
-            <CreateProjectModal />
-            <CreateTaskModal />
-            <CreateSubtaskModal />
+            <WrapperComponent>
+                {children}
+                <CreateProjectModal />
+                <CreateTaskModal />
+                <CreateSubtaskModal />
+            </WrapperComponent>
+
             <Footer />
         </>
     )
