fe41db6e51bd0d24e770e8a0f05b1f1de696f79a
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-11 17:16:46 +0100

    Use cards in subtasks

--- components/Subtasks/Subtasks.tsx
+++ components/Subtasks/Subtasks.tsx
@@ -1,26 +1,39 @@
+import { Button, Card, CardActions, CardContent, Grid, Typography } from "@material-ui/core";
 import Link from "next/link";
 import React, { useContext } from "react";
 import styled from "styled-components";
 import { ResourceContext } from "../../contexts/ResourceContext";
 
-const SubtasksWrapper = styled.div``;
-const Subtask = styled.div`
-    
+const SubtasksWrapper = styled.div`
+    margin-top: 12px;
 `;
 
-export const Subtasks = ({ subtasks }: { subtasks: {id: string, name: string }[]}) => {
+export const Subtasks = ({ subtasks }: { subtasks: { id: string, name: string }[] }) => {
     const { activeProjectId } = useContext(ResourceContext);
     if (!subtasks?.length) {
         return null;
     }
     return (
         <SubtasksWrapper>
-            {subtasks.map(subtask => (
-                <Subtask>
-                    {subtask.name}
-                    <Link href={`/project/${activeProjectId}/task/${subtask.id}`}>Visit</Link>
-                </Subtask>
-            ))}
+            <h2>Subtasks</h2>
+            <Grid container>
+                {subtasks.map(subtask => (
+                    <Grid item xs={12} md={3}>
+                        <Card>
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    {subtask.name}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Link href={`/project/${activeProjectId}/task/${subtask.id}`}>
+                                    <Button size="small">Visit</Button>
+                                </Link>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </SubtasksWrapper>
     )
 }
