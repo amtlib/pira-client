f10d6ba31246256d8f3544bd54341b8a61d5134d
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-11 17:16:26 +0100

    Fix button position

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Link from 'next/link';
 import { UserContext } from '../../contexts/UserContext';
@@ -10,16 +9,27 @@ import { Avatar, IconButton, Menu, MenuItem } from '@material-ui/core';
 import { Logo } from '../Logo/LogoWrapper';
 import { ModalContext } from '../../contexts/ModalContext';
 import { ResourceContext } from '../../contexts/ResourceContext';
+import { BackButton } from '../BackButton/BackButton';
 
 const useStyles = makeStyles(() => ({
     title: {
-        flexGrow: 1,
+        // flexGrow: 1,
         cursor: "pointer",
         height: "30px",
     },
     avatar: {
         backgroundColor: "#ff0000",
     },
+    buttons: {
+        marginLeft: "auto",
+        '& > *': {
+            margin: "10px",
+        },
+    },
+    toolbar: {
+        width: "100%",
+        gap: "40px"
+    }
 }));
 
 const Navbar = ({ page }: { page?: "index" | "project" | "task" | "projectSettings" | "login" | "register" | "projects" }) => {
@@ -41,64 +51,69 @@ const Navbar = ({ page }: { page?: "index" | "project" | "task" | "projectSettin
 
     return (
         <AppBar position="static">
-            <Toolbar>
+            <Toolbar className={classes.toolbar}>
                 <Link href="/">
                     <div className={classes.title}>
                         <Logo />
                     </div>
                 </Link>
-                <div>
+                {(page === "task" || page === "projectSettings") && (
+                    <BackButton />
+                )}
+                <div className={classes.buttons}>
                     {page === "projects" && (
                         <Button variant="contained" onClick={() => setIsCreateProjectModalOpen(true)}>create project</Button>
                     )}
                     {page === "project" && (
                         <>
-                            <Button variant="contained" onClick={() => setIsCreateTaskModalOpen(true)}>create task</Button>
-                            <Link href={`/project/${activeProjectId}/settings`}><Button variant="contained">project settings</Button></Link>
+                            <Button variant="contained" color="secondary" onClick={() => setIsCreateTaskModalOpen(true)}>create task</Button>
+                            <Link href={`/project/${activeProjectId}/settings`}>
+                                <Button variant="outlined" color="default">project settings</Button>
+                            </Link>
                         </>
                     )}
                     {page === "task" && (
-                        <Button variant="contained" onClick={() => setIsCreateSubtaskModalOpen(true)}>create subtask</Button>
+                        <Button variant="contained" color="secondary" onClick={() => setIsCreateSubtaskModalOpen(true)}>create subtask</Button>
+                    )}
+                    {loggedIn ? (
+                        <>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={showMenu}
+                                color="inherit"
+                            >
+                                <Avatar aria-label="poll" className={classes.avatar}>
+                                    {firstName?.substring(0, 1).toUpperCase() || email?.substring(0, 1).toUpperCase()}
+                                </Avatar>
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={() => setAnchorEl(null)}
+                            >
+                                <MenuItem onClick={handleLogOut}>Log out</MenuItem>
+                            </Menu>
+                        </>
+                    ) : (
+                        <Link href="/login">
+                            <Button color="inherit" >
+                                Log in
+                            </Button>
+                        </Link>
                     )}
                 </div>
-                {loggedIn ? (
-                    <div>
-                        <IconButton
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={showMenu}
-                            color="inherit"
-                        >
-                            <Avatar aria-label="poll" className={classes.avatar}>
-                                {firstName?.substring(0, 1).toUpperCase() || email?.substring(0, 1).toUpperCase()}
-                            </Avatar>
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={open}
-                            onClose={() => setAnchorEl(null)}
-                        >
-                            <MenuItem onClick={handleLogOut}>Log out</MenuItem>
-                        </Menu>
-                    </div>
-                ) : (
-                    <Link href="/login">
-                        <Button color="inherit" >
-                            Log in
-                        </Button>
-                    </Link>
-                )}
             </Toolbar>
         </AppBar>
     );
